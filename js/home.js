00deae8fe7a1ef17b41f4de9a3ed5c616684853d
ShafayatSadid <mdshafayathossainsadid@gmail.com>
2026-03-07 17:14:50 +0200

    card data fetched okay design okay

---
+++ js/home.js
@@ -0,0 +1,82 @@
+console.log('home added')
+
+// load all card
+const loadAll = () => {
+
+    const url = 'https://phi-lab-server.vercel.app/api/v1/lab/issues';
+    fetch(url)
+        .then(response => response.json())
+        .then(cards => displayAll(cards.data))
+}
+
+loadAll()
+
+// display all cards
+
+const displayAll = (cards) => {
+
+    console.log(cards.length)
+    // declare all issue count
+    const allIssueCount = document.getElementById('all-issue');
+    const allCount = cards.length;
+    allIssueCount.innerText = allCount;
+
+
+    // get parent and empty
+    const allSection = document.getElementById('all-section');
+    allSection.innerHTML = "";
+
+    cards.forEach(card => {
+        // console.log(card)
+
+        
+        // create element and added innerHTMl
+        // append child
+
+        const allCard = document.createElement('div');
+
+        // call priority design function.
+        const priorityData = getPriority(card.priority.toUpperCase())
+
+        allCard.innerHTML = `
+        <div class="card w-[256px] h-[290px] p-4 shadow-sm space-y-3">
+
+                    <!-- status and priority -->
+                    <div class="flex justify-between">
+                        <div>${card.status == 'open' ?'<img class="w-6 h-6" src="./assets/Open-Status.png" alt="">': '<img class="w-6 h-6" src="./assets/Closed- Status .png" alt="">'} </div>
+                        <button id=""
+                            class="priority-btn text-[0.75rem] ${priorityData.textColor} w-[80px] py-[5px] rounded-[100px] ${priorityData.bgColor}">${priorityData.text} </button>
+                    </div>
+
+                    <!-- title -->
+                    <div class="space-y-2">
+                        <h4 class="text-[0.875rem] font-semibold text-[#1F2937]">${card.title}</h4>
+                        <p class="text-[0.75rem] text-[#64748B]">${card.description}</p>
+                    </div>
+
+                    <!-- labels -->
+                    <div class="flex items-center gap-2">
+                        <button
+                            class="text-[0.75rem] flex items-center gap-1 text-[#EF4444] p-[5px] rounded-[100px] bg-[#FEECEC]"><img
+                                class="w-3 h-3" src="./assets/bug.png" alt="">bug</button>
+
+                        <button
+                            class="text-[0.75rem] flex items-center gap-1 text-[#D97706] p-[5px] rounded-[100px] bg-[#FFF8DB]"><img
+                                class="w-3 h-3" src="./assets/help.png" alt=""> HELP WANTED</button>
+                    </div>
+
+                    <!-- author -->
+                    <div class="p-4 border-t border-[#E4E4E7]">
+
+                        <div>
+                            <p class="text-[0.75rem] text-[#64748B]">#${card.id} ${card.author} </p>
+                            <p class="text-[0.75rem] text-[#64748B]">${card.createdAt} </p>
+                        </div>
+                    </div>
+                </div>
+        `
+
+        allSection.appendChild(allCard)
+        
+    });
+}
--- js/machine.js
+++ js/machine.js
@@ -5,3 +5,32 @@ function getValue (id){
     const value = input.value;
     return value;
 }
+
+// function for design priority
+function getPriority(priority){
+
+    if(priority === 'HIGH'){
+
+        return{
+            text: 'HIGH',
+            bgColor: 'bg-[#FEECEC]',
+            textColor: 'text-[#EF4444]'
+        }
+    }
+    else if(priority === 'MEDIUM'){
+
+        return{
+            text: 'MEDIUM',
+            bgColor: 'bg-[#FFF6D1]',
+            textColor: 'text-[#F59E0B]'
+        }
+    }
+    else{
+
+        return{
+            text: 'LOW',
+            bgColor: 'bg-[#EEEFF2]',
+            textColor: 'text-[#9CA3AF]'
+        }
+    }
+}
